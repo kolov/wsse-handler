9871435c2095268dcb867effecda0ac53a187457
Assen Kolov <assen@rife.powerhouse.local>
2015-01-27 16:22:25 +0100

    clean up

--- src/main/java/com/akolov/wsse/WssePasswordDigestHandler.java
+++ src/main/java/com/akolov/wsse/WssePasswordDigestHandler.java
@@ -5,42 +5,32 @@ import org.apache.ws.security.message.WSSecHeader;
 import org.apache.ws.security.message.WSSecUsernameToken;
 
 import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPEnvelope;
-import javax.xml.soap.SOAPHeader;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.util.HashSet;
 import java.util.Set;
 
 public class WssePasswordDigestHandler implements SOAPHandler<SOAPMessageContext> {
 
 
-    public void sign(SOAPMessage message) throws Exception {
-        SOAPPart soappart = message.getSOAPPart();
-        SOAPEnvelope envelope = soappart.getEnvelope();
-        SOAPHeader header = envelope.getHeader();
-        WSSecHeader wsheader = new WSSecHeader();
-        wsheader.insertSecurityHeader(soappart);
-        WSSecUsernameToken token = new WSSecUsernameToken();
-        token.setPasswordType(WSConstants.PASSWORD_DIGEST);
-        token.setUserInfo("myuser", "mypass");
-        token.build(soappart, wsheader);
-    }
-
     @Override
     public Set<QName> getHeaders() {
-        return null;
+        Set<QName> HEADERS = new HashSet<QName>();
+
+        HEADERS.add(new QName(WSConstants.WSSE_NS, "Security"));
+        HEADERS.add(new QName(WSConstants.WSSE11_NS, "Security"));
+
+        return HEADERS;
     }
 
     @Override
     public boolean handleMessage(SOAPMessageContext context) {
         try {
-            // got the message from the context
             SOAPMessage msg = context.getMessage();
 
-            // is outgoing?
             Boolean isOutGoing = (Boolean) context
                 .get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
 
@@ -54,15 +44,25 @@ public class WssePasswordDigestHandler implements SOAPHandler<SOAPMessageContext
         return true;
     }
 
-
     @Override
     public boolean handleFault(SOAPMessageContext context) {
         return true;
     }
 
+
     @Override
     public void close(MessageContext context) {
 
+    }
+
+    private void sign(SOAPMessage message) throws Exception {
+        SOAPPart soappart = message.getSOAPPart();
+        WSSecHeader wsheader = new WSSecHeader();
+        wsheader.insertSecurityHeader(soappart);
+        WSSecUsernameToken token = new WSSecUsernameToken();
+        token.setPasswordType(WSConstants.PASSWORD_DIGEST);
+        token.setUserInfo("myuser", "mypass");
+        token.build(soappart, wsheader);
     }
 
 }
